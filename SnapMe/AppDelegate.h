087441aabfd64ddae463d322eff029c48b7c11cd
wendyfly <wendyliao01@gmail.com>
2016-01-05 23:31:26 -0800

    Initial Commit

---
+++ SnapMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapMe
+//
+//  Created by LiaoWenwen on 1/5/16.
+//  Copyright © 2016 LiaoWenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnapMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnapMe
+//
+//  Created by LiaoWenwen on 1/5/16.
+//  Copyright © 2016 LiaoWenwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
