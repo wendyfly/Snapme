1b4fd178e10565df964406d1746a2b56207fb95e
wendyfly <wendyliao01@gmail.com>
2016-01-07 22:33:35 -0800

    Implement login function, but back to rootview cannot work

--- SnapMe/LoginViewController.m
+++ SnapMe/LoginViewController.m
@@ -8,6 +8,7 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
 
@@ -35,4 +36,79 @@
 }
 */
 
+- (IBAction)login:(id)sender {
+    NSString *username = [self.usernameField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet: [NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    if(username.length == 0 || password.length == 0 ) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle: @"Oops" message: @"Please fill the username and password field" preferredStyle: UIAlertControllerStyleAlert];
+        UIAlertAction* ok = [UIAlertAction
+                             actionWithTitle:@"OK"
+                             style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction * action)
+                             {
+                                 [alert dismissViewControllerAnimated:YES completion:nil];
+                                 
+                             }];
+        UIAlertAction* cancel = [UIAlertAction
+                                 actionWithTitle:@"Cancel"
+                                 style:UIAlertActionStyleDefault
+                                 handler:^(UIAlertAction * action)
+                                 {
+                                     [alert dismissViewControllerAnimated:YES completion:nil];
+                                     
+                                 }];
+        
+        [alert addAction:ok];
+        [alert addAction:cancel];
+        [self presentViewController:alert animated:YES completion:nil];
+        
+    } else {
+        [PFUser logInWithUsernameInBackground:username password:password
+                                        block:^(PFUser *user, NSError *error) {
+                                            if (user) {
+                                                NSLog(@"Successfully");
+                                                UIAlertController *alert = [UIAlertController alertControllerWithTitle: @"Congratulations" message: @"Successfully" preferredStyle: UIAlertControllerStyleAlert];
+                                                UIAlertAction* ok = [UIAlertAction
+                                                                     actionWithTitle:@"OK"
+                                                                     style:UIAlertActionStyleDefault
+                                                                     handler:^(UIAlertAction * action)
+                                                                     {
+                                                                         [alert dismissViewControllerAnimated:YES completion:nil];
+                                                                         
+                                                                     }];
+                                                [alert addAction:ok];
+                                                [self presentViewController:alert animated:YES completion:nil];
+                                                
+                                                [self.navigationController popToRootViewControllerAnimated:true];
+                                                
+                                                
+                                            } else {
+                                                NSLog(@"fail");
+                                                UIAlertController *alertView = [UIAlertController alertControllerWithTitle: @"Oops" message: @"username and password is dismatched" preferredStyle: UIAlertControllerStyleAlert];
+                                                UIAlertAction* ok = [UIAlertAction
+                                                                     actionWithTitle:@"OK"
+                                                                     style:UIAlertActionStyleDefault
+                                                                     handler:^(UIAlertAction * action)
+                                                                     {
+                                                                         [alertView dismissViewControllerAnimated:YES completion:nil];
+                                                                         
+                                                                     }];
+                                                UIAlertAction* cancel = [UIAlertAction
+                                                                         actionWithTitle:@"Cancel"
+                                                                         style:UIAlertActionStyleDefault
+                                                                         handler:^(UIAlertAction * action)
+                                                                         {
+                                                                             [alertView dismissViewControllerAnimated:YES completion:nil];
+                                                                             
+                                                                         }];
+                                                
+                                                [alertView addAction:ok];
+                                                [alertView addAction:cancel];
+                                                [self presentViewController:alertView animated:YES completion:nil];
+                                                
+                                            }
+                                        }];
+    }
+}
 @end
